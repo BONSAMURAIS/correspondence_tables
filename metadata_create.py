661eb1ee8e3dd4ac093998f22bd23c4df2c173a4
unknown <miguelfastudillo@gmail.com>
2019-10-26 15:41:03 +0200

    move files

--- metadata_create.py
+++
@@ -1,7 +0,0 @@
-import datapackage
-package = datapackage.Package()
-package.infer('final_tables/tables/*.csv')
-print(package.descriptor)
-print(package.valid)
-print(package.errors)
-package.save('final_tables/tables/datapackage.json')
