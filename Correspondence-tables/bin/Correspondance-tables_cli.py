1fa82fa98fdc138e8e8ccd5f00a7f1512b85ddaf
unknown <miguelfastudillo@gmail.com>
2019-11-20 20:32:43 +0100

    add_cli

---
+++ Correspondence-tables/bin/Correspondance-tables_cli.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Correspondance-tables CLI.
+
+Usage:
+  Correspondance-tables-cli regenerate <dirpath>
+
+Options:
+  -h --help     Show this screen.
+  --version     Show version.
+
+"""
+from docopt import docopt
+from Correspondence-tables import generate_all
+import sys
+
+
+def main():
+    try:
+        args = docopt(__doc__, version='0.2')
+        generate_all(args['<dirpath>'])
+    except KeyboardInterrupt:
+        print("Terminating CLI")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
